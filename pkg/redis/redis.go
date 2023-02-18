8285b16e513eb2ec79779564f7e5182ad580de2c
leexsh <851001285@qq.com>
2023-02-18 21:02:52 +0800

    feat: complete the encapsulation of redis

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -72,3 +72,99 @@ func (r *Redis) Exists(k string) int64 {
 	}
 	return r.singleClient.Exists(context.Background(), k).Val()
 }
+
+// Keys redis 查找所有符合给定模式的所有key
+func (r *Redis) Keys(k string) []string {
+	if r.clusterMode {
+		return r.cluster.Keys(context.Background(), k).Val()
+	}
+	return r.singleClient.Keys(context.Background(), k).Val()
+}
+
+// Del redis删除所有指定key的所有数据
+func (r *Redis) Del(k string) error {
+	if r.clusterMode {
+		return r.cluster.Del(context.Background(), k).Err()
+	}
+	return r.singleClient.Del(context.Background(), k).Err()
+}
+
+// Exprie redis设置key过期时间
+func (r *Redis) Expire(k string, t time.Duration) error {
+	if r.clusterMode {
+		return r.cluster.Expire(context.Background(), k, t).Err()
+	}
+	return r.singleClient.Expire(context.Background(), k, t).Err()
+}
+
+// Set redis用string的格式存储key值
+func (r *Redis) Set(k, v string, t time.Duration) error {
+	if r.clusterMode {
+		return r.cluster.Set(context.Background(), k, v, t).Err()
+	}
+	return r.singleClient.Set(context.Background(), k, v, t).Err()
+}
+
+// SetNx 不存在则写入
+func (r *Redis) SetNx(k, v string, t time.Duration) error {
+	if r.clusterMode {
+		return r.cluster.SetNX(context.Background(), k, v, t).Err()
+	}
+	return r.singleClient.SetNX(context.Background(), k, v, t).Err()
+}
+
+// Get redis用string的方式获取key
+func (r *Redis) Get(k string) string {
+	if r.clusterMode {
+		return r.cluster.Get(context.Background(), k).Val()
+	}
+	return r.singleClient.Get(context.Background(), k).Val()
+}
+
+// HSet redis用hash的方式存储key的field字段
+func (r *Redis) HSet(k, field string, value interface{}) error {
+	if r.clusterMode {
+		return r.cluster.HSet(context.Background(), k, field, value).Err()
+	}
+	return r.singleClient.HSet(context.Background(), k, field, value).Err()
+}
+
+// HGet redis用hash的形式读取key的field字段
+func (r *Redis) HGet(k, field string) string {
+	if r.clusterMode {
+		return r.cluster.HGet(context.Background(), k, field).Val()
+	}
+	return r.singleClient.HGet(context.Background(), k, field).Val()
+}
+
+// HMSet redis用hash的方式存储key的field字段
+func (r *Redis) HMSet(k, field string, value interface{}) error {
+	if r.clusterMode {
+		return r.cluster.HMSet(context.Background(), k, field, value).Err()
+	}
+	return r.singleClient.HMSet(context.Background(), k, field, value).Err()
+}
+
+// HMGet redis用hash的形式读取key的field字段
+func (r *Redis) HMGet(k, field string) []interface{} {
+	if r.clusterMode {
+		return r.cluster.HMGet(context.Background(), k, field).Val()
+	}
+	return r.singleClient.HMGet(context.Background(), k, field).Val()
+}
+
+// HDel redis删除hash散列表key的field字段
+func (r *Redis) HDel(k, field string) error {
+	if r.clusterMode {
+		return r.cluster.HDel(context.Background(), k, field).Err()
+	}
+	return r.singleClient.HDel(context.Background(), k, field).Err()
+}
+
+// HGetAll redis读取hash散列表key值对应的全部字段数据
+func (r *Redis) HGetAll(k string) map[string]string {
+	if r.clusterMode {
+		return r.cluster.HGetAll(context.Background(), k).Val()
+	}
+	return r.singleClient.HGetAll(context.Background(), k).Val()
+}
